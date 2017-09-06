1a591d74542c3bc7a07d7161277ba0d3a1e4759f
Meghea Iulian <iulian.meghea@gmail.com>
2017-09-06 18:48:19 +0300

    Atom

--- modules/development/atom.nix
+++ modules/development/atom.nix
@@ -2,34 +2,14 @@
 {
   nixpkgs.overlays = [( self: super: {
     atom = super.atom.overrideAttrs(oldAttrs: rec {
-      name = "atom-1.18.0";
-      version = "1.18.0";
+      name = "atom-${version}";
+      version = "1.19.5";
 
       src = super.fetchurl {
-        url = "https://github.com/atom/atom/releases/download/v1.18.0/atom-amd64.deb";
-        sha256 = "07hssch8sfyp5sji91lx4v62m8zmy9j971i968p747dwfp6g0my6";
-        name = "atom-1.18.0.deb";
+        url = "https://github.com/atom/atom/releases/download/v${version}/atom-amd64.deb";
+        sha256 = "04zkyn6zrkdr71h8hh0lakfk3hvr2jclbj4l3dlk7rvmrzqp900b";
+        name = "${name}.deb";
       };
-
-      buildCommand = ''
-        mkdir -p $out/usr/
-        ar p $src data.tar.xz | tar -C $out -xJ ./usr
-        substituteInPlace $out/usr/share/applications/atom.desktop \
-          --replace /usr/share/atom $out/bin
-        mv $out/usr/* $out/
-        rm -r $out/share/lintian
-        rm -r $out/usr/
-        wrapProgram $out/bin/atom \
-         --prefix "PATH" : "${super.gvfs}/bin"
-        fixupPhase
-        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-          --set-rpath "${super.atomEnv.libPath}:$out/share/atom" \
-        $out/share/atom/atom
-        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-          --set-rpath "${super.atomEnv.libPath}" \
-        $out/share/atom/resources/app/apm/bin/node
-        find $out/share/atom -name "*.node" -exec patchelf --set-rpath "${super.atomEnv.libPath}:$out/share/atom" {} \;
-      '';
     });
   })];
 
