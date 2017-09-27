34f41378bcdf17b273f56da18b6206b62cdd3914
Meghea Iulian <iulian.meghea@gmail.com>
2017-09-27 20:47:06 +0300

    Gitkraken 3.0.2

--- modules/development/gitkraken.nix
+++ modules/development/gitkraken.nix
@@ -1,19 +1,29 @@
 { config, lib, pkgs, ... }:
 {
+  #all = import <nixpkgs> {};
   nixpkgs.overlays = [( self: super: {
+     gkCurl = super.callPackage <nixpkgs/pkgs/tools/networking/curl> rec {
+      fetchurl = super.fetchurlBoot;
+      http2Support = true;
+      zlibSupport = true;
+      scpSupport = true;
+      gnutlsSupport = true;
+    };
     gitkraken = super.gitkraken.overrideAttrs(oldAttrs: rec {
-      version = "2.7.0";
+      version = "3.0.2";
       name    = "gitkraken-${version}";
 
       src = super.fetchurl {
         url = "https://release.gitkraken.com/linux/v${version}.deb";
-        sha256 = "0088vdn47563f0v9zhk1vggn3c2cfg8rhmifc6nw4zbss49si5gp";
+        sha256 = "0ryzgarw1f4m6xgp1f1dc7m3h3rm9hyaj7r21091m2xkh8b3f9kn";
       };
     
       libPath = super.stdenv.lib.makeLibraryPath [
         super.stdenv.cc.cc.lib
+        self.gkCurl
         super.curl
         super.udev
+        super.gnutls
         super.xorg.libX11
         super.xorg.libXext
         super.xorg.libXcursor
@@ -44,46 +54,26 @@
         super.gnome2.GConf
         super.libgnome_keyring
       ];
-
-      nativeBuildInputs = [ super.makeWrapper ];
-
-      dontBuild = true;
-
-      desktopItem = super.makeDesktopItem {
-        name = "gitkraken";
-        exec = "gitkraken";
-        icon = "app";
-        desktopName = "GitKraken";
-        genericName = "Git Client";
-        categories = "Application;Development;";
-        comment = "Graphical Git client from Axosoft";
-      };
-
       buildInputs = [ super.dpkg ];
+      unpackPhase = "true";
+      buildCommand = ''
+        mkdir -p $out
+        dpkg -x $src $out
+        cp -av $out/usr/* $out
+        rm -rf $out/etc $out/usr $out/share/lintian
+        chmod -R g-w $out
 
-      unpackPhase = "dpkg-deb -x $src .";
-
-      installPhase = ''
-        mkdir -p "$out/opt/gitkraken"
-        cp -r usr/share/gitkraken/* "$out/opt/gitkraken"
-
-        mkdir -p "$out/share/applications"
-        cp $desktopItem/share/applications/* "$out/share/applications"
-
-        mkdir -p "$out/share/pixmaps"
-        cp usr/share/pixmaps/app.png "$out/share/pixmaps"
+        for file in $(find $out -type f \( -perm /0111 -o -name \*.so\* \) ); do
+          patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$file" || true
+          patchelf --set-rpath ${libPath}:$out/share/gitkraken $file || true
+        done
+        
+        find $out/share/gitkraken -name "*.node" -exec patchelf --set-rpath "${libPath}:$out/share/gitkraken" {} \; 
+        
+        rm $out/bin/gitkraken
+        ln -s $out/share/gitkraken/gitkraken $out/bin/gitkraken
       '';
-
-      postFixup = ''
-        patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) \
-                --set-rpath "$libPath:$out/opt/gitkraken" "$out/opt/gitkraken/gitkraken"
-        wrapProgram $out/opt/gitkraken/gitkraken \
-          --prefix LD_PRELOAD : "${super.stdenv.lib.makeLibraryPath [ super.curl ]}/libcurl.so.4" \
-          --prefix LD_PRELOAD : "${super.stdenv.lib.makeLibraryPath [ super.libgnome_keyring ]}/libgnome-keyring.so.0"
-        mkdir "$out/bin"
-        ln -s "$out/opt/gitkraken/gitkraken" "$out/bin/gitkraken"
-      '';
-    });   
+      }); 
    })];
 
   environment.systemPackages = with pkgs; [
