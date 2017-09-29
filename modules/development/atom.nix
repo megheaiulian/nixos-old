c14a81ff75661e9a361b5ba0a5a9c3a0dc146b28
Meghea Iulian <iulian.meghea@gmail.com>
2017-09-29 21:40:44 +0300

    Update atom

--- modules/development/atom.nix
+++ modules/development/atom.nix
@@ -3,11 +3,11 @@
   nixpkgs.overlays = [( self: super: {
     atom = super.atom.overrideAttrs(oldAttrs: rec {
       name = "atom-${version}";
-      version = "1.19.5";
+      version = "1.20.1";
 
       src = super.fetchurl {
         url = "https://github.com/atom/atom/releases/download/v${version}/atom-amd64.deb";
-        sha256 = "04zkyn6zrkdr71h8hh0lakfk3hvr2jclbj4l3dlk7rvmrzqp900b";
+        sha256 = "0mr82m3yv18ljai3r4ncr65bqhjwxyf1si77iza4ijk5zv1llp7i";
         name = "${name}.deb";
       };
     });
