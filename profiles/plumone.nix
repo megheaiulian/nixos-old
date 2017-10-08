87d2bb07d323870a01c9d1188e8b37206eac578f
Meghea Iulian <iulian.meghea@gmail.com>
2017-10-08 11:40:45 +0300

    Groups setup

--- profiles/plumone.nix
+++ profiles/plumone.nix
@@ -10,25 +10,21 @@
 
   users = {
     defaultUserShell = "/run/current-system/sw/bin/fish";
+    groups.iulian = {
+      gid = 1000;
+    };
     users.iulian = {
       isNormalUser = true;
       uid = 1000;
-      extraGroups = ["wheel" "disk" "audio" "video" "networkmanager" "systemd-journal" "lxd" ];
+      extraGroups = ["iulian" "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal" "lxd" ];
       initialPassword = "iulian";
     };
   };
 
-  networking.firewall.allowedTCPPorts = [9988];
   environment.systemPackages = with pkgs; [
     transmission_gtk
-    comical
     lastpass-cli
   ];
-  
-  #swapDevices = [{
-  #  size      = 2048;
-  #  priority  = 5; 
-  #}];
   
   zramSwap = {
     enable    = true;
