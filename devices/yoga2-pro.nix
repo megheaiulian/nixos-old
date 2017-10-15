e98c5d295ea459a0293f318c25c07cf2b4436705
Meghea Iulian <iulian.meghea@gmail.com>
2017-10-15 10:15:37 +0300

    Fix yoga fan control

--- devices/yoga2-pro.nix
+++ devices/yoga2-pro.nix
@@ -21,7 +21,14 @@
         rev    = "a3c5af53a7dd63c3a5e8034757233f362fed5a5e";
         sha256 = "1pwz3y3dkcnjkisv3r7kd8zwlgjlc8y45md1h7n8ypyvpx2m85n0";
       };
+
 	    buildInputs = [ super.cmake ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp yoga_fan $out/bin/
+      '';
+
     };
   })];
 
